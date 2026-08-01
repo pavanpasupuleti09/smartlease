45b5f30447e08e52844c200193dcec7403fdf561
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-08-01 22:45:45 +0530

    feat: add login request and response DTOs

---
+++ src/main/java/com/smartlease/auth/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.smartlease.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String message;
+}
