7cf7496e60b89716e919c680db079f4279de0f5f
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-08-16 20:46:51 +0530

    feat: complete backend APIs and Razorpay payment integration

--- src/main/java/com/smartlease/payment/dto/PaymentVerifyRequest.java
+++ src/main/java/com/smartlease/payment/dto/PaymentVerifyRequest.java
@@ -1,5 +1,6 @@
 package com.smartlease.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,12 @@ import lombok.Setter;
 @Setter
 public class PaymentVerifyRequest {
 
+    @JsonAlias("razorpay_order_id")
     private String razorpayOrderId;
+
+    @JsonAlias("razorpay_payment_id")
     private String razorpayPaymentId;
+
+    @JsonAlias("razorpay_signature")
     private String razorpaySignature;
 }
